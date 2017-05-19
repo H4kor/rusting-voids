5d2989d7915e1e988e1293e1df2c752bfeddaaac
Niko Abeler <me@h4kor.de>
2017-05-19 13:45:21 +0200

    using termion for output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+extern crate termion;
 mod celestial;
+
+use termion::{color, style};
 
 use celestial::Body;
 
 fn main() {
     let s = celestial::Star{ _mass: 100., _radius: 34.0, lumosity: 0.64 };
-    println!("Mass: {:?}", s.mass());
-    println!("Radius: {:?}", s.radius());
-    println!("Lumosity: {:?}", s.lumosity);
-    println!("Volume: {:?}", s.volume());
-    println!("Density: {:?}", s.density());
+    println!("Mass: {}{:?}{}", color::Fg(color::Red), s.mass(), color::Fg(color::Reset));
+    println!("Radius: {}{:?}{}", color::Fg(color::Red), s.radius(), color::Fg(color::Reset));
+    println!("Lumosity: {}{:?}{}", color::Fg(color::Red), s.lumosity, color::Fg(color::Reset));
+    println!("Volume: {}{:?}{}", color::Fg(color::Red), s.volume(), color::Fg(color::Reset));
+    println!("Density: {}{:?}{}", color::Fg(color::Red), s.density(), color::Fg(color::Reset));
 }
