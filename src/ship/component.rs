3b9c6b13a26f8cc5d8d679e079b833fcdcf330d9
Niko Abeler <me@h4kor.de>
2017-05-28 23:04:06 +0200

    tree-like ship contruction

--- src/ship/component.rs
+++ src/ship/component.rs
@@ -16,8 +16,19 @@ pub trait ShipComponent {
     /// positive -> produces energy
     fn power_mod(&self) -> f64;
 
+    /// changes power mode of the device the power supply
+    fn toggle_power(&mut self) {
+
+    }
+    
+    fn timestep(&mut self, t: &Time) {
+        self._timestep(t);
+    }
+
     /// executes temporal actions 
-    fn timestep(&mut self, t: &Time);
+    /// implement this for each component
+    /// will be executed by `timestep`
+    fn _timestep(&mut self, t: &Time);
 }
 
 struct CargoBay {
@@ -34,7 +45,43 @@ impl ShipComponent for CargoBay {
         0.0
     }
 
-    fn timestep(&mut self, t: &Time) {
+    fn _timestep(&mut self, t: &Time) {
 
     }
+}
+
+/// Allows attaching further ShipComponent
+pub struct Connector {
+    pub max: usize,
+    pub comps: Vec<Box<ShipComponent>>
+}
+
+impl Connector {
+    fn addComponent<T: ShipComponent + 'static>(&mut self, c: T) {
+        if self.comps.len() < self.max {
+            self.comps.push(Box::new(c));
+        }
+    }
+
+}
+
+impl ShipComponent for Connector {
+    fn short_desc(&self) -> String {
+        format!("Connector")
+    }
+
+    fn power_mod(&self) -> f64 {
+        let mut p = 0.0;
+        for ref comp in &self.comps {
+            p += comp.power_mod();
+        }
+        p 
+    }
+
+    fn _timestep(&mut self, t: &Time) {
+        for ref mut comp in &mut self.comps {
+            comp.timestep(t);
+        }
+    }
+
 }
--- src/ship/core.rs
+++ src/ship/core.rs
@@ -2,42 +2,33 @@
 use std::vec::Vec;
 use std::borrow::BorrowMut;
 use std::ops::DerefMut;
-use ship::component::ShipComponent;
+use ship::component::{Connector, ShipComponent};
 
 use utils::time::Time;
 
 pub struct ShipCore {
-    comps: Vec<Box<ShipComponent>>,
+    main_connector: Connector,
 }
 
 impl ShipCore {
     pub fn new() -> ShipCore {
-        ShipCore{comps: vec!()}
+        ShipCore{
+            main_connector: Connector{
+                max: 4,
+                comps: vec!()
+            }
+        }
     }
 
     /// executes temporal actions 
     pub fn timestep(&mut self, t: &Time) {
-        for i in 0..self.getNumComps() {
-            let comp = self.getCompByPos(i);
-            comp.timestep(t);
-        }
+        self.main_connector.timestep(t);
     }
 
     pub fn power(&self) -> f64 {
         // basic power supply
         let mut p = 10.0;
-        for comp in &self.comps {
-            p += comp.as_ref().power_mod()
-        }
+        p += self.main_connector.power_mod();
         p
     }
-    
-    fn getNumComps(&self) -> usize {
-        self.comps.len()
-    }
-
-    fn getCompByPos(&mut self, i: usize) -> &mut ShipComponent {
-        self.comps[i].deref_mut() 
-    }
-
 }
